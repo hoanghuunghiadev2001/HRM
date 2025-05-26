f067a9ea9acb6ca50447db5aabb9d1f231c17d86
hoanghuunghiadev2001 <hoanghuunghia.dev@gmail.com>
2025-05-26 15:07:47 +0700

    fix report

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50  data-[state=active]:shadow-sm data-[state=active]:bg-blue-600 data-[state=active]:text-white h-10 px-4 mr-4",
+      "inline-flex items-center border border-[#999999] justify-center whitespace-nowrap rounded-sm py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50  data-[state=active]:shadow-sm data-[state=active]:bg-blue-600 data-[state=active]:text-white h-10 px-4 mr-4",
       className
     )}
     {...props}
