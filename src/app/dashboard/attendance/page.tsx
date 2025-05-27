3c4e57472608c90c53c07ce4d24e7839bc0a74f1
hoanghuunghiadev2001 <hoanghuunghia.dev@gmail.com>
2025-05-27 14:39:33 +0700

    fix attendance

--- src/app/dashboard/attendance/page.tsx
+++ src/app/dashboard/attendance/page.tsx
@@ -1,8 +1,6 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
-
-import { useEffect, useState, useCallback, useMemo } from "react";
+import { useEffect, useState } from "react";
 import "../../globals.css";
 
 import React from "react";
@@ -22,6 +20,7 @@ import { formatDateTime } from "@/components/function";
 import { createStyles } from "antd-style";
 import dayjs from "dayjs";
 import { fetchAttendances } from "@/lib/api";
+import { AttendanceResponse } from "@/lib/interface";
 import Image from "next/image";
 import { UploadOutlined } from "@ant-design/icons";
 
@@ -41,7 +40,7 @@ interface DataType {
 
 const useStyle = createStyles((utils) => {
   const { css, token } = utils;
-  const antCls = (token as any).antCls || ".ant";
+  const antCls = (token as any).antCls || ".ant"; // fallback nếu token.antCls không tồn tại
 
   return {
     customTable: css`
@@ -66,7 +65,7 @@ function getTodayVNDateString() {
   const localOffset = now.getTimezoneOffset();
   const diff = vietnamOffset + localOffset;
   now.setMinutes(now.getMinutes() + diff);
-  return now.toISOString().split("T")[0];
+  return now.toISOString().split("T")[0]; // YYYY-MM-DD
 }
 
 export default function AttendancePage() {
@@ -74,81 +73,27 @@ export default function AttendancePage() {
   const [pageSize, setPageSize] = useState(10);
   const [pageTable, setPageTable] = useState(1);
   const [totalTable, setTotalTable] = useState(0);
-  const [listAttendance, setListAttendance] = useState<any>();
   const localUser = getUserFromLocalStorage();
-
-  // State tập trung filter
-  const [filters, setFilters] = useState({
-    name: "",
-    msnv: "",
-    department: "",
-    dateRange: [null, null] as [string | null, string | null],
-  });
-
+  const [filterName, setFilterName] = useState("");
+  const [filterMSNV, setFilterMSNV] = useState("");
+  const [filterDepartment, setFilterDepartment] = useState("");
   const todayVN = getTodayVNDateString();
 
+  const [timeStart, setTimeStart] = useState("");
+  const [timeEnd, setTimeEnd] = useState("");
+  const [listAttendance, setListAttendance] = useState<AttendanceResponse>();
+
+  const { RangePicker } = DatePicker;
+  const maxDate = dayjs();
   const { styles } = useStyle();
 
-  // Hàm fetch dữ liệu
-  const handleFetchAttendances = useCallback(
-    async (page: number, size: number) => {
-      setLoading(true);
-      try {
-        const res = await fetchAttendances({
-          msnv: filters.msnv,
-          name: filters.name,
-          department:
-            localUser.role === "ADMIN"
-              ? filters.department
-              : localUser.workInfo.department,
-          fromDate: filters.dateRange[0],
-          toDate: filters.dateRange[1],
-          page,
-          pageSize: size,
-        });
-        if (res.status === 1) {
-          setListAttendance(res.data);
-          setTotalTable(res.data?.total ?? 0);
-        }
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    },
-    [filters, localUser, pageSize]
-  );
-
-  // Gọi fetch khi component mount hoặc page, size thay đổi
-  useEffect(() => {
-    handleFetchAttendances(pageTable, pageSize);
-  }, [handleFetchAttendances, pageTable, pageSize]);
-
-  // Biến chuyển đổi dữ liệu table
-  const listAttendanceFormat = useMemo(() => {
-    return (listAttendance?.data || []).map((item: any, index: string) => ({
-      key: (index + 1 + (pageTable - 1) * pageSize).toString(),
-      employeeId: item.employeeId,
-      employeeCode: item.employeeCode,
-      avatar: item.avatar,
-      employeeName: item.employeeName,
-      department: item.department,
-      position: item.position,
-      date: formatDateTime(item.date),
-      firstCheckIn: formatDateTime(item.firstCheckIn),
-      lastCheckOut: item.lastCheckOut ? formatDateTime(item.lastCheckOut) : "",
-      totalHours: item.totalHours,
-    }));
-  }, [listAttendance, pageTable, pageSize]);
-
-  // Mảng columns cho Table
   const columns: TableProps<DataType>["columns"] = [
     {
       title: "STT",
       dataIndex: "key",
       rowScope: "row",
       width: "60px",
-      render: (_, record) => <p>{record.key}</p>,
+      render: (_) => <p>{_}</p>,
     },
     {
       title: "MSNV",
@@ -164,7 +109,6 @@ export default function AttendancePage() {
       render: (_, record) => (
         <div className="flex items-center gap-2">
           <Image
-            loading="lazy"
             src={record.avatar ? record.avatar : "/storage/avt-default.png"}
             alt="avt"
             className="h-8 w-8 rounded-[50%]"
@@ -207,59 +151,133 @@ export default function AttendancePage() {
     },
   ];
 
-  // Xử lý bỏ qua
-  const changeDate = (dates: any, dateStrings: any) => {
-    setFilters((prev) => ({
-      ...prev,
-      dateRange: [dateStrings[0], dateStrings[1]],
-    }));
-  };
-
-  // Hàm tìm kiếm, refresh dữ liệu
-  const handleSearch = () => {
-    setPageTable(1); // reset trang về 1
-    handleFetchAttendances(1, pageSize);
-  };
-
-  // Hàm đổi trang
-  const onPageChange = (page: number, size?: number) => {
-    if (size) setPageSize(size);
-    setPageTable(page);
-  };
-
-  // Xuất excel
-  const handleExportExcel = async () => {
-    try {
-      const res = await fetch("/api/attendance/export", {
-        method: "POST",
-        body: JSON.stringify({
-          week: todayVN,
-          department: localUser.role === "ADMIN" ? filters.department : "",
-        }),
-        headers: { "Content-Type": "application/json" },
-      });
-      if (!res.ok) {
-        throw new Error("Failed to export");
-      }
-      const blob = await res.blob();
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      a.href = url;
-      a.download = "employees.xlsx";
-      a.click();
-      URL.revokeObjectURL(url);
-    } catch (error) {
-      console.error("Export error:", error);
+  const handleFetchAttendances = async (
+    pageTable: number,
+    pageSize: number
+  ) => {
+    setLoading(true);
+    const res = await fetchAttendances({
+      msnv: filterMSNV,
+      name: filterName,
+      department:
+        localUser.role === "ADMIN"
+          ? filterDepartment
+          : localUser.workInfo.department,
+      fromDate: timeStart,
+      toDate: timeEnd,
+      page: pageTable,
+      pageSize,
+    });
+    if (res.status === 1) {
+      setListAttendance(res.data);
+      setTotalTable(res.data?.total ?? 1);
+      setLoading(false);
+    } else {
+      setLoading(false);
     }
+  };
+
+  // //xem chi tiết đơn xin nghỉ
+  // const DetailRequetsLeave = (id: number) => {
+  //   const item = allRequestsApproved?.data?.find((item) => item.id === id);
+  //   setInfoRequestLeave(item);
+  //   setModalDetailLeave(true);
+  // };
+
+  // // chức năng phê duyệt đơn xin nghỉ
+  // const putApprovedRequest = async (
+  //   id: number | string,
+  //   statusRequest: string
+  // ) => {
+  //   setLoading(true);
+  //   try {
+  //     await approveLeaveRequest(id, statusRequest, localUser.name);
+  //     await getApiAllRequestsApproved(pageTable, pageSize);
+  //     await getApiAllRequestsNeed();
+  //     setLoading(false);
+  //   } catch (err) {
+  //     console.error("Lỗi:", err);
+  //     setLoading(false);
+  //   }
+  // };
+
+  const onPageChange = (page: number, pageSizeEnter?: number) => {
+    if (pageSizeEnter) {
+      setPageSize(pageSizeEnter);
+      handleFetchAttendances(page, pageSizeEnter);
+    } else {
+      setPageTable(page);
+      handleFetchAttendances(page, pageSize);
+    }
+  };
+
+  useEffect(() => {
+    handleFetchAttendances(pageTable, pageSize);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  //style table scroll
+
+  // const { data, isLoading, error } = useAttendances(
+  //   {
+  //     msnv: filterMSNV,
+  //     name: filterName,
+  //     deparment: localUser.role === "ADMIN" ? "" : localUser.deparment,
+  //     fromDate: timeStart,
+  //     toDate: timeEnd,
+  //     page: pageTable,
+  //     pageSize,
+  //   },
+  //   enabledSearch || initialLoad
+  // );
+
+  const formatted: DataType[] =
+    listAttendance?.data.map((item, index) => ({
+      key: (index + 1).toString(),
+      employeeId: item.employeeId,
+      employeeCode: item.employeeCode,
+      avatar: item.avatar,
+      employeeName: item.employeeName,
+      department: item.department,
+      position: item.position,
+      date: formatDateTime(item.date),
+      firstCheckIn: formatDateTime(item.firstCheckIn),
+      lastCheckOut: item.lastCheckOut
+        ? formatDateTime(item.lastCheckOut ?? "")
+        : "",
+
+      totalHours: item.totalHours,
+    })) || [];
+
+  const changeDate = (value: any, dateString: any) => {
+    setTimeStart(dateString[0]);
+    setTimeEnd(dateString[1]);
+  };
+
+  const handleExportExcel = async () => {
+    const res = await fetch("/api/attendance/export", {
+      method: "POST",
+      body: JSON.stringify({
+        week: todayVN, // Ngày bất kỳ trong tuần muốn xuất
+        department: localUser.role === "ADMIN" ? "" : localUser.department, // Hoặc bỏ trống nếu xuất toàn bộ
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const blob = await res.blob();
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "employees.xlsx";
+    a.click();
+    URL.revokeObjectURL(url);
   };
 
   return (
     <div>
       <ModalLoading isOpen={loading} />
 
-      {/* Tiêu đề và export */}
-      <div className="w-full flex justify-between mb-4">
-        <p className="font-bold text-2xl text-[#4a4a6a]">
+      <div className="w-full flex justify-between">
+        <p className="font-bold  text-2xl text-[#4a4a6a]">
           Danh sách chấm công:
         </p>
         <Button
@@ -270,78 +288,81 @@ export default function AttendancePage() {
           <p className="hidden sm:block">Xuất file tuần này</p>
         </Button>
       </div>
-
-      {/* Bộ lọc */}
-      <div className="w-full mb-4">
-        <p className="font-bold text-xl text-[#4a4a6a] mb-2">Tìm kiếm:</p>
-        <div className="grid grid-cols-2 md:flex md:items-center gap-4 mb-4 px-4">
-          {/* MSNV */}
+      <div className="w-full">
+        <p className="font-bold  text-xl text-[#4a4a6a]">Tìm kiếm:</p>
+        <div className="grid grid-cols-2 md:flex md:items-center gap-4 mb-4 w-full mt-2 px-4 flex-wrap">
           <div className="flex gap-2 items-center">
             <Form.Item
-              label={<p className="font-bold hidden md:block">MSNV</p>}
+              label={
+                <p className="font-bold text-[#242424] hidden md:block">MSNV</p>
+              }
             >
               <Input
+                className=" w-full md:!w-[80px]"
                 placeholder="MSNV"
-                className="w-full md:!w-[80px]"
-                value={filters.msnv}
-                onChange={(e) =>
-                  setFilters((prev) => ({ ...prev, msnv: e.target.value }))
-                }
-                onKeyDown={(e) => e.key === "Enter" && handleSearch()}
+                onChange={(e) => setFilterMSNV(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    handleFetchAttendances(pageTable, pageSize);
+                  }
+                }}
               />
             </Form.Item>
           </div>
-
-          {/* Tên NV */}
-          <div className="flex gap-2 items-center">
+          <div className="flex gap-2 items-center ">
+            {/* <p className="text-sm text-[#4a4a6a] shrink-0">Tên NV:</p> */}
             <Form.Item
-              label={<p className="font-bold hidden md:block">Tên NV</p>}
+              label={
+                <p className="font-bold text-[#242424] hidden md:block">
+                  Tên NV
+                </p>
+              }
             >
               <Input
-                placeholder="Tên NV"
                 className="w-full md:!w-[100px]"
-                value={filters.name}
-                onChange={(e) =>
-                  setFilters((prev) => ({ ...prev, name: e.target.value }))
-                }
-                onKeyDown={(e) => e.key === "Enter" && handleSearch()}
+                placeholder="Tên NV"
+                onChange={(e) => setFilterName(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    handleFetchAttendances(pageTable, pageSize);
+                  }
+                }}
               />
             </Form.Item>
           </div>
-
-          {/* Theo ngày */}
-          <div className="flex gap-2 items-center col-span-2">
+          <div className="!flex gap-2 items-center col-span-2">
             <Form.Item
-              label={<p className="font-bold hidden md:block">Theo ngày</p>}
+              name="range-picker"
+              label={
+                <p className="font-bold text-[#242424] hidden md:block">
+                  Theo ngày
+                </p>
+              }
             >
-              <DatePicker
+              <RangePicker
                 format={"DD/MM/YYYY"}
-                style={{ width: "100%" }}
+                lang="vn"
                 onChange={changeDate}
-                disabledDate={(current) => current && current > dayjs()}
-                value={
-                  filters.dateRange[0] && filters.dateRange[1]
-                    ? [dayjs(filters.dateRange[0]), dayjs(filters.dateRange[1])]
-                    : []
-                }
+                disabledDate={(current) => {
+                  return current && current > maxDate;
+                }}
               />
             </Form.Item>
           </div>
-
-          {/* Bộ phận nếu là admin */}
-          {localUser?.role === "ADMIN" && (
-            <div className="flex gap-2 items-center">
+          {localUser?.role === "ADMIN" ? (
+            <div className="!flex gap-2 items-center ">
               <Form.Item
-                label={<p className="font-bold hidden md:block">Bộ phận</p>}
+                label={
+                  <p className="font-bold text-[#242424] hidden md:block">
+                    Bộ phận
+                  </p>
+                }
               >
                 <Select
-                  placeholder="Bộ phận"
+                  onChange={(e) => setFilterDepartment(e)}
+                  className="w-full md:!w-[100px]"
+                  placeholder={"Bộ phận"}
                   allowClear
-                  style={{ width: "100%" }}
-                  value={filters.department}
-                  onChange={(value) =>
-                    setFilters((prev) => ({ ...prev, department: value }))
-                  }
                   options={[
                     { value: "KD", label: "KD" },
                     { value: "SCC", label: "SCC" },
@@ -356,36 +377,34 @@ export default function AttendancePage() {
                 />
               </Form.Item>
             </div>
+          ) : (
+            ""
           )}
-
-          {/* Nút tìm */}
           <button
             className="flex shrink-0 gap-2 items-center h-8 px-4 rounded-lg bg-gradient-to-r from-[#4c809e] to-[#001935] cursor-pointer text-white font-semibold"
-            onClick={handleSearch}
+            onClick={() => {
+              handleFetchAttendances(pageTable, pageSize);
+            }}
           >
             Tìm kiếm
           </button>
         </div>
-      </div>
-
-      {/* Bảng */}
-      <Table<DataType>
-        className={styles.customTable}
-        columns={columns}
-        dataSource={listAttendanceFormat}
-        scroll={{ y: "calc(100vh - 335px)", x: "100%" }}
-        pagination={false}
-      />
-
-      {/* Phân trang */}
-      <div className="flex justify-center mt-3">
+        <Table<DataType>
+          className={styles.customTable}
+          columns={columns}
+          dataSource={formatted ?? []}
+          scroll={{ y: "calc(100vh - 335px)", x: "100%" }}
+          pagination={false}
+        />
         <Pagination
-          current={pageTable}
+          align="center"
+          // current={pageTable}
           pageSize={pageSize}
           total={totalTable}
           onChange={onPageChange}
           showSizeChanger
           onShowSizeChange={onPageChange}
+          className="!mt-3"
         />
       </div>
     </div>
